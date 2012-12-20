cde107aae72a525de0b8fb9a9b48d889cc8d5aad
corey_reil <corey_reil@americancentury.com>
2012-12-19 19:16:38 -0600

    initial commit

---
+++ spring-batch-ftp/src/test/java/org/reil/example/ExampleItemWriterTests.java
@@ -0,0 +1,15 @@
+package org.reil.example;
+
+import org.reil.example.ExampleItemWriter;
+
+import junit.framework.TestCase;
+
+public class ExampleItemWriterTests extends TestCase {
+
+	private ExampleItemWriter writer = new ExampleItemWriter();
+	
+	public void testWrite() throws Exception {
+		writer.write(null); // nothing bad happens
+	}
+
+}
